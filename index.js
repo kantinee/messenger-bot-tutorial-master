f5f77ff9c92c9de941b11d8c58178451932a691b
kantinee <kantinee@gmail.com>
2017-03-25 22:00:18 +0700

    ying

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.post('/webhook/', function (req, res) {
 			//child.on('exit', function() {
 			//  process.exit()
 			//})
-			child = exec('/usr/bin/java -jar ~/TunTREND_M1_SentimentCurrent11.jar' ,
+			child = exec('/usr/bin/java -jar ~/TunTREND_M1_SentimentCurrent11.jar ' + fntext,
 			  function (error, stdout, stderr){
 			    console.log('stdout: ' + stdout);
 			    console.log('stderr: ' + stderr);
