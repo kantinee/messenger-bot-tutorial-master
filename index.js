e793582deafbc87462c756411e0e65f78cc1629c
kantinee <kantinee@gmail.com>
2017-03-25 15:43:33 +0700

    ying

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.post('/webhook/', function (req, res) {
 			//----Test HandY
 			var exec = require('child_process').exec, child;
 			sendTextMessage(sender, "ying Processing..." + fntext) 
-			child = exec('/usr/bin/java -jar ~/TunTREND_M1_SentimentCurrent.jar fntext parameter /wait' ,
+			child = exec('/usr/bin/java -jar ~/TunTREND_M1_SentimentCurrent.jar fntext' ,
 			  function (error, stdout, stderr){
 			    console.log('stdout: ' + stdout);
 			    console.log('stderr: ' + stderr);
